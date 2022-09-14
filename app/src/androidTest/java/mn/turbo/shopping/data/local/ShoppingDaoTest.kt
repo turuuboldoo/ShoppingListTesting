9d4e4818f7aa14b6c04793c98be6b109698dfbbf
turuuboldoo <turuu.boldoo@gmail.com>
2022-09-14 16:49:44 +0800

    hilt module in androidTest

--- app/src/androidTest/java/mn/turbo/shopping/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/mn/turbo/shopping/data/local/ShoppingDaoTest.kt
@@ -1,41 +1,39 @@
 package mn.turbo.shopping.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
-import mn.turbo.shopping.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import mn.turbo.shopping.getOrAwaitValue
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaoTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: ShoppingItemDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
     private lateinit var dao: ShoppingDao
 
     @Before
     fun setup() {
-        database = Room
-            .inMemoryDatabaseBuilder(
-                ApplicationProvider.getApplicationContext(),
-                ShoppingItemDatabase::class.java
-            )
-            .allowMainThreadQueries()
-            .build()
-
+        hiltRule.inject()
         dao = database.shoppingDao
     }
 
